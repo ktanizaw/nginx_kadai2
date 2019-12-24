5b3163c65f26d85edcaeb4a335b3b12e1cdeabd1
tanizawa_kenta <kt10025102@gmail.com>
2019-12-24 16:15:36 +0900

    update

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,25 +1,20 @@
 class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :destroy]
 
-  # GET /blogs
   def index
     @blogs = Blog.all
   end
 
-  # GET /blogs/1
   def show
   end
 
-  # GET /blogs/new
   def new
     @blog = Blog.new
   end
 
-  # GET /blogs/1/edit
   def edit
   end
 
-  # POST /blogs
   def create
     @blog = Blog.new(blog_params)
 
@@ -30,7 +25,6 @@ class BlogsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /blogs/1
   def update
     if @blog.update(blog_params)
       redirect_to @blog, notice: 'Blog was successfully updated.'
@@ -39,19 +33,16 @@ class BlogsController < ApplicationController
     end
   end
 
-  # DELETE /blogs/1
   def destroy
     @blog.destroy
     redirect_to blogs_url, notice: 'Blog was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_blog
       @blog = Blog.find(params[:id])
     end
 
-    # Only allow a trusted parameter "white list" through.
     def blog_params
       params.require(:blog).permit(:title, :content)
     end
--- config.ru
+++ config.ru
@@ -1,6 +1,23 @@
 # This file is used by Rack-based servers to start the application.
 
-require_relative 'config/environment'
+# require_relative 'config/environment'
+#
+# run Rails.application
+# run Proc.new {[200,{'Content-Type' => 'text/plain'}, ["hello world"]]}
 
-run Rails.application
-run Proc.new {[200,{'Content-Type' => 'text/plain'}, ["hello world"]]}
+# This file is used by Rack-based servers to start the application.
+# RAILS_RELATIVE_URL_ROOT="/${bl_path}"
+# require ::File.expand_path('../config/environment',  __FILE__)
+# if RAILS_RELATIVE_URL_ROOT then
+#         map RAILS_RELATIVE_URL_ROOT do
+#                 run Rails.application
+#         end
+# else
+#         run Rails.application
+# end
+
+
+require ::File.expand_path('../config/environment', __FILE__)
+map Rails.application.config.relative_url_root || '/' do
+  run Rails.application
+end
